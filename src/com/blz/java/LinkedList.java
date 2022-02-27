1f8c276c52453dc222bb82a437de3023c745e285
Kirandhanwai <kirandhanwai123@gmail.com>
2022-02-28 00:52:26 +0530

    Added : Ablility to Insert

--- src/com/blz/java/LinkedList.java
+++ src/com/blz/java/LinkedList.java
@@ -1,7 +1,10 @@
 package com.blz.java;
 
+import java.util.Scanner;
+
 public class LinkedList {
 	Node head;
+	Scanner scanner = new Scanner(System.in);
 
 	public class Node {
 		Object data;
@@ -40,6 +43,77 @@ public class LinkedList {
 		}
 	}
 
+	public void pop() {
+		if (head == null)
+			System.out.println("Nothing to delete..");
+		else {
+			head = head.ref;
+		}
+	}
+
+	public void popLast() {
+
+		if (head == null)
+			System.out.println("No elements to delete");
+		else if (head.ref == null)
+			head = null;
+		else {
+			Node temp = head;
+
+			while (temp.ref.ref != null) {
+				temp = temp.ref;
+			}
+			temp.ref = null;
+		}
+	}
+
+	public void search(Object data) {
+
+		boolean isFound = false;
+
+		if (head == null)
+			System.out.println("No elements to search");
+
+		else if (head.data == data)
+			isFound = true;
+
+		else {
+
+			Node temp = head;
+			while (temp != null) {
+
+				if (temp.data == data) {
+					isFound = true;
+					break;
+				}
+
+				temp = temp.ref;
+			}
+		}
+
+		if (isFound)
+			System.out.println("Data found ");
+		else
+			System.out.println(data + " not found");
+	}
+
+	public void insertNode(Object data) {
+		Node newNode = new Node(data);
+
+		if (head == null)
+			head = newNode;
+		else {
+			Node temp = head;
+			System.out.println("Enter the data after which new data should be added");
+			Object data1 = scanner.nextInt();
+			while (temp.data != data1) {
+				temp = temp.ref;
+			}
+			newNode.ref = temp.ref;
+			temp.ref = newNode;
+		}
+	}
+
 	public void display() {
 		if (head == null)
 			System.out.println("No elements to display.");
@@ -52,7 +126,6 @@ public class LinkedList {
 					System.out.println(temp.data);
 				temp = temp.ref;
 			}
-
 		}
 	}
 }
--- src/com/blz/java/LinkedListMain.java
+++ src/com/blz/java/LinkedListMain.java
@@ -3,11 +3,14 @@ package com.blz.java;
 public class LinkedListMain {
 	public static void main(String[] args) {
 		LinkedList list = new LinkedList();
-		System.out.println("Append nodes");
+		System.out.println("Insert node in between");
 		list.addLast(56);
-		list.addLast(30);
+		// list.addLast(30);
 		list.addLast(70);
 		list.display();
+		list.insertNode(30);
+		list.display();
+
 	}
 }
 
